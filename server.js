3cc6631a0f0c5a16c9ab691a864ebb7a2ef5acab
Chirag Chavda <chiragchavda1737@gmail.com>
2026-03-15 21:54:18 +0530

    fix mongodb connection

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
 
 const authRoutes = require("./routes/authRoutes");
@@ -10,16 +11,33 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("AI Fitness Backend Running 🚀");
+
+// ================= MONGODB CONNECTION =================
+
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+console.log("MongoDB Connected");
+})
+.catch(err=>{
+console.log(err);
 });
 
-app.use("/api/auth", authRoutes);
-app.use("/api", contactRoutes);
-app.use("/api", workoutRoutes);
+
+// ================= ROUTES =================
+
+app.get("/", (req,res)=>{
+res.send("AI Fitness Backend Running 🚀");
+});
+
+app.use("/api/auth",authRoutes);
+app.use("/api",contactRoutes);
+app.use("/api",workoutRoutes);
+
+
+// ================= SERVER =================
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-console.log("Server running on port " + PORT);
+app.listen(PORT,()=>{
+console.log("Server running on port "+PORT);
 });
